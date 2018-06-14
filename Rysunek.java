2d2e7ebbb6ff2c43a514e221271f2b0137f32bad
dawidmyk <dawidmyk@wp.pl>
2018-06-14 15:32:49 +0200

    Drzewo nieBSTT

--- Rysunek.java
+++ Rysunek.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
+import java.util.*;
 class Pusta extends JComponent {
 	int W; //szerokość całego obrazka
 	int H; //wysokość całego obrazka
@@ -20,34 +21,103 @@ class Pusta extends JComponent {
 	}
 	
 }
-
+class Vertex extends Point {
+	Vertex parent;
+	Vertex leftSon;
+	Vertex rightSon;
+	boolean white;
+	boolean whichi;
+	Vertex(int x, int y) {
+		super(x, y);
+		white = false;
+		whichi = false;
+	}
+	void connect(Vertex other, int side) {
+		parent = other;
+		if(side == 0) {
+			whichi = false;
+			other.leftSon = this;
+		}
+		else {
+			whichi = true;
+			other.rightSon = this;
+		}
+	}
+	void comeLine(Graphics rysunek, int r) {
+		rysunek.setColor(Color.BLUE);
+		if(leftSon!=null) rysunek.drawLine(x, y, leftSon.x, leftSon.y);
+		if(rightSon!=null) rysunek.drawLine(x, y, rightSon.x, rightSon.y);
+		rysunek.setColor(Color.GREEN);
+		rysunek.fillOval(x-r, y-r, 2*r, 2*r);
+		rysunek.setColor(Color.RED);
+		rysunek.drawOval(x-r, y-r, 2*r, 2*r);
+	}
+}
+	
+	
+	
 
 class Drzewo extends Pusta {
 	int x; //szerokość na najniższym poziomie
 	int y; //wysokość jednego poziomu
 	int h; //wysokość drzewa w poziomach
 	int r; //promień kółka z liczbą
+	ArrayList Points;
+	Drzewo (int params[]) {
+		this(params[0], params[1], params[2], params[3]);
+	}
 	Drzewo (int x, int y, int h, int r) {
-		this.x = x;
-		this.y = y;
+		this.x = (x+r)*2;
+		this.y = y+2*r; //żeby nie było za mało
 		this.h = h;
 		this.r = r;
-		H = h*y + 3*r;
-		W = ((int)Math.pow(2, h-1))*x + 3*r;
+		Points = new ArrayList();
+		H = h*this.y + 4*r;
+		W = ((int)Math.pow(2, h))*this.x + 4*r;
 		super.setDim(W, H);
-	}	
+	}
+	void count_points() {
+		Points.clear();
+		int k = 1;
+		int generalCount = 0;
+		int element = W - 4*r;
+		for(int i = -1; i!=h; i++) {
+			element = element/2;
+			for(int j = 0; j!=k; j++) {
+				int x = 2*r + element*(2*j+1);
+				int y = 2*r + this.y*(i+1);
+				Vertex node = new Vertex(x, y);
+				int place = generalCount - j - k/2 + j/2;
+				if(i!=-1) node.connect((Vertex) Points.get(place), j%2);
+				Points.add(node);
+				generalCount++;
+				
+			}
+			k = k*2;
+		}
 		
-		
-		
-		
+		System.out.println(W);
+		System.out.println(H);
+		ListIterator ire = Points.listIterator();
+		while(ire.hasNext()) {
+			System.out.println(ire.next());
+		}
+	 //debugowanie
+	}
+	
 		
 	public void paintComponent(Graphics rysunek) {
-	
+		super.paintComponent(rysunek);
+		ListIterator ire = Points.listIterator();
+		while(ire.hasNext()) ((Vertex) ire.next()).comeLine(rysunek, r);
+			
 	}
 }
-class Zestaw extends JPanel {
+class Zestaw extends JPanel implements ActionListener {
 	 JPanel naZmienne[];
 	 JButton naRysuj;
+	 JScrollPane naDrzewo;
+	 Drzewo drzewko;
 	Zestaw() {
 		String labels[] = {"x", "y", "h", "r"};
 		naZmienne = new JPanel[4];
@@ -55,13 +125,39 @@ class Zestaw extends JPanel {
 			naZmienne[n] = new JPanel();
 			naZmienne[n].setLayout(new BorderLayout());
 			naZmienne[n].add(new JLabel(labels[n]), "North");
-			naZmienne[n].add(new TextField(), "South");
+			naZmienne[n].add(new JTextField(), "South");
 			naZmienne[n].setPreferredSize(new Dimension(30, 30));
 			add(naZmienne[n]);
 		}
 		naRysuj = new JButton("Rysuj");
+		naRysuj.addActionListener(this);
 		add(naRysuj);
 	}
+	void connect(JScrollPane naDrzewo) {
+		this.naDrzewo = naDrzewo;
+	}
+	 public void actionPerformed(ActionEvent e) {
+		 System.out.println("Widzę");
+		 if(e.getSource()==naRysuj) {
+			 int params[] = new int[4];
+			 for(int i = 0; i!=4; i++) {
+				 for(Component comp: naZmienne[i].getComponents()) {
+					 if(comp instanceof JTextField) 
+					 params[i] = Integer.parseInt(((JTextField) comp).getText());
+					 System.out.println("Tutaj");
+					 System.out.println(comp);
+
+				}
+			}
+			for(int i : params) System.out.println(i);
+			naDrzewo.setViewportView(drzewko = new Drzewo(params));
+			drzewko.count_points();
+			naDrzewo.repaint();
+			 
+		 }
+	 }
+		
+		
 }
 	
 
@@ -70,7 +166,7 @@ class Zestaw extends JPanel {
 
 
 
-class Rysunek extends JFrame implements ActionListener {
+class Rysunek extends JFrame {
 	 Container cp = getContentPane();
 	 JScrollPane naDrzewo;
 	 Zestaw naReszte;
@@ -85,9 +181,11 @@ class Rysunek extends JFrame implements ActionListener {
 		 setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		 pack();
 		 setVisible(true);
+		 //Drzewo drzewko = new Drzewo(15, 20, 3, 5);
+		 naReszte.connect(naDrzewo);
+		 //drzewko.count_points();
 	 }
-	 public void actionPerformed(ActionEvent e) {
-	 }
+	
 	 public static void main(String Ara[]) {
 		 new Rysunek();
 	 }
